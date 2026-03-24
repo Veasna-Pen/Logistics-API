cd200f904f8c8f5240fc48cf9b2c64d6c0a80c6d
Veasna3d <veasna3d@gmail.com>
2026-03-24 13:38:47 +0700

    day4

---
+++ database/migrations/2026_03_24_063050_create_shipments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipments', function (Blueprint $table) {
+            $table->id();
+            $table->string('tracking_code')->unique();
+            $table->string('sender_name');
+            $table->string('receiver_name');
+            $table->string('pickup_address');
+            $table->string('delivery_address');
+            $table->string('status')->index();
+            $table->foreignId('assign_deliver_id')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete()
+                ->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipments');
+    }
+};
---
+++ database/migrations/2026_03_24_063519_create_status_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipment_status_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('shipment_id')
+                ->constrained('shipments')
+                ->cascadeOnDelete();
+            $table->string('status');
+            $table->foreignId('updated_by')
+                ->constrained('users')
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipment_status_logs');
+    }
+};
